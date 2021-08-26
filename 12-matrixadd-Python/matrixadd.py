d993ab7b96a01e87e2097ba9dc5a9016bd9e30ef
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 16:48:06 +0530

    Done

--- 12-matrixadd-Python/matrixadd.py
+++ 12-matrixadd-Python/matrixadd.py
@@ -16,5 +16,9 @@
 # cannot be added because they are of different dimensions.
 
 def matrixadd(L, M):
-	# Your code goes here
-	pass
+    if len(L[0])==len(M[0]) == 1:
+        return [[L[0][0]+M[0][0]]]
+    elif (len(L) != len(M) or len(L[0]) != len(M[0]) or len(L[1]) != len(M[1])):
+        return None
+    result = [[L[i][j] + M[i][j]  for j in range(len(L[0]))] for i in range(len(L))]
+    return result
