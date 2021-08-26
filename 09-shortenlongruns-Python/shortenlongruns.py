a93dc4d155dc918380566d2fc982a0d05d29e0a0
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 12:14:11 +0530

    Done

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -10,5 +10,24 @@
 
 
 def shortenlongruns(L, k):
-	# Your code goes here
-	pass
+    # Your code goes here
+    new_lis = []
+    dicti = {}
+    for i in range(len(L)):
+        if L[i] in dicti:
+            if (dicti[L[i]]<k-1 and i != len(L)-1) and (L[i] == L[i+1] or L[i-1] == L[i]):
+                dicti[L[i]] += 1
+                new_lis.append(L[i])
+            elif i==len(L)-1 and L[i-1] == L[i] and dicti[L[i]] < k-1:
+                dicti[L[i]] += 1
+                new_lis.append(L[i])
+            elif L[i-1] != L[i]:
+                dicti[L[i]] = 1
+                new_lis.append(L[i])
+            else:
+                dicti[L[i]] =1
+        else:
+            dicti[L[i]] = 1
+            new_lis.append(L[i])
+            
+    return new_lis
