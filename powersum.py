6ac2dc0a37855ae1ccf28ec0976604c674834c43
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 11:31:13 +0530

    Done

--- powersum.py
+++ powersum.py
@@ -15,11 +15,18 @@ def power(a, b):
         return 1
     else:
         return a * power(a, b-1)
-def powerSum(n, k):
+ 
+ 
+def powerSum(n, k,sum=0):
     # Your code goes here...
-    
-    return power(n,k)
+    if n == 1:
+        return sum+1
+    else:
+        sum += power(n,k)
+        return powerSum(n-1,k,sum)
+ 
+print(powerSum(3,10))
 # Write your own test cases here...
-assert(powerSum(2,10) == 1024)
-assert(powerSum(3,10) == 59049)
+assert(powerSum(2,10) == 1025)
+assert(powerSum(3,10) == 60074)
 print ("All test cases passed...")
