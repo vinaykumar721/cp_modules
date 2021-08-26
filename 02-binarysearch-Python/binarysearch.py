d19f0b2882ca2b4e3dd39bc77183e55e014cc015
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 13:04:27 +0530

    Done

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -11,5 +11,16 @@ Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
 def binary_search(input_array, value):
-    # Your code goes here
-    pass
+    input_array.sort()
+    first=0
+    last = len(input_array)-1
+    while first <= last:
+        mid = (last+first)//2
+        if input_array[mid] == value:
+            return mid
+        elif(value < input_array[mid]):
+            last = mid-1
+        else:
+            first=mid+1
+    return -1
+
