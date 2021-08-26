77f444e623a27a6c04db2c3f613b7c09361767e2
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 12:43:11 +0530

    Done

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,30 @@
 
 
 
-def fun_nth_happy_prime(n):
-	return 0
+def nth_happy_number(n):
+    if n==1:
+        return 1
+    if n==2:
+        return 7
+ 
+    a=2;b=8
+    while a<=n:
+        if ishappy(b):
+            a+=1
+        if a==n:
+            return b
+        b+=1
+def ishappy(m):
+    while(m>=10):
+        m=squarenum(m)
+        if(m==1):
+            return True
+    return False
+ 
+def squarenum(b):
+    sum=0
+    while(b>0):
+        rem=b%10
+        sum+=(rem*rem)
+        b//=10
+    return sum 
