df980dddc908b52c3a6a6b4a8fc09ab65c8e3105
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 12:07:39 +0530

    Done

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -5,5 +5,37 @@
 # The two closest numbers in that list are 2 and 6, and their difference is 4.
 
 def smallestdifference(a):
-	# Your code goes here
-	pass
+    # return empty to the null input
+    if not a:
+        return []
+    k=[]
+    check= a[0]
+    count=0
+    # checking for the dupilcate values and counting them 
+    for i in a:
+        if i==check:
+            count+=1
+        else:
+            check =i
+            if(count>0):
+                k.append(count)
+            count=1
+    k.append(count)
+    chk=a[0]
+    s=a[0]
+    c=[s]
+    # list the values of repeated to invidual.
+    for i in a:
+        
+        if(i == chk):
+            # c.append(s)
+            continue
+        else:            
+            chk=i
+            c.append(chk)
+    
+    # print(c)
+    # print(a)
+    # print(k)
+    z=list(zip(k,c))
+    return z
