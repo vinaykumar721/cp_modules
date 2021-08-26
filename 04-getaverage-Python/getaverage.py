99258e83bed37b81ed599f0ab483da9ac744c15c
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 17:28:52 +0530

    Done

--- 04-getaverage-Python/getaverage.py
+++ 04-getaverage-Python/getaverage.py
@@ -10,5 +10,15 @@
 
 
 def fun_getaverage(s): 
-	return 0.0
+    sum=0
+    base=0
+    for i in s.split(","):
+        if i.isnumeric():
+            sum+=float(i)
+            base+=1
+ 
+    if base==0:
+        return 0.0
+    return sum/base
+
 
