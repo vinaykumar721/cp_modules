5a1f87458330cdae32aac94801f679b6f60ce817
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 12:00:32 +0530

    Done

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -4,7 +4,22 @@
 
 
 
+import math
 def fun_kth_occurrences(s, n):
-	return 'a'
-
+    dicti = dict()
+    for i in s:
+        if i in dicti:
+            dicti[i] += 1
+        elif i.isspace():
+            pass
+        else:
+            dicti[i] = 1
+    count = 1
+    while count < n:
+        max_num = max(dicti.values())
+        for k,v in dicti.items():
+            if max_num == v:
+                dicti[k] = 0
+                count+=1
+    return max(dicti,key=dicti.get)
 
