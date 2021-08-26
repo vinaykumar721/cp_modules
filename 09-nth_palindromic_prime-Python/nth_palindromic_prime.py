8d2852423b97996665861b33fccd7b680a967afd
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 13:08:05 +0530

    Done

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -7,5 +7,37 @@
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) 
 # returns 2
 
-def fun_nth_palindromic_prime(n):
-	return 0
+def isPalindromicPrime(a):
+    if (isPalindrome(a) and isPrime(a)):
+        return True
+    else:
+        return False
+ 
+def isPalindrome(n):
+    n=str(n)
+    return n[::-1]==n
+ 
+def isPrime(n):
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    else:
+        for i in range(3, n):
+            if n % i == 0:
+                return False
+        return True
+ 
+assert (isPalindromicPrime(2) == True)
+assert (isPalindromicPrime(10) == False)
+assert (isPalindromicPrime(104) == False)
+assert (isPalindromicPrime(235) == False)
+assert (isPalindromicPrime(131) == True)
+assert (isPalindromicPrime(900) == False)
+assert (isPalindromicPrime(101) == True)
+assert (isPalindromicPrime(383) == True)
+assert (isPalindromicPrime(373) == True)
+assert (isPalindromicPrime(121) == False)
+print("All test cases passed... :-)")
