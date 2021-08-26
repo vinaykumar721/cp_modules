257f1144a4479ae6f7f9caada1985a621411e143
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 12:21:08 +0530

    Done

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -4,9 +4,19 @@
 # two matrices cannot be multiplied for any reason.
 
 
-def fun_matrixmultiply(m1, m2):
-    return None
-
+def multipolynomials(p1, p2):
+    q=len(p1)
+    r=len(p2)
+    c=[]
+    m=0
+    if(q==r==0):
+        return None
+    while(m<len(p1)):
+        for i in range(len(p2)):
+            if(p1[m]*p2[i]!=0):
+                c.append(p1[m]*p2[i])
+        m+=1
+    return c
 
 
 
