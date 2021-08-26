3a97248d80920e514fc283c037c6efe5fccde39b
vinaykumar721 <gamanab163@msitprogram.net>
2021-08-26 12:26:42 +0530

    Done

--- 07-digitcount-Python/digitcount.py
+++ 07-digitcount-Python/digitcount.py
@@ -6,5 +6,11 @@
 # repeatedly remove the ones digit until you cannot.
 
 def digitcount(n):
-	# your code goes here
-	pass
+    n=abs(n)
+    if n==0:
+        return 1
+    count=0
+    while n>0:
+        count+=1
+        n//=10
+    return count
