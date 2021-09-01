346fa33e16a3c60bebda18b5bc9ffc654f09511e
vinaykumar721 <gamanab163@msitprogram.net>
2021-09-01 09:26:36 +0530

    Done

--- isKingsTour.py
+++ isKingsTour.py
@@ -18,6 +18,44 @@
 # returns True if it represents a legal Kings Tour 
 # and False otherwise.
 
-def isKingsTour(board):
-    # Your code goes here...
-    pass
+def isKnightsTour(board):
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            if (board[x][y] == 0):
+                return False
+    i=1
+    while(i<(len(board)*len(board[0]))):
+        r1,c1 = rowcol(board,i)
+        r2,c2 = rowcol(board,(i+1))
+        if(r1 == -1) or (c1 == -1):
+            return False
+        i+=1
+        if(isvalidmove(r1,c1,r2,c2) == False):
+            return False
+    return True 
+ 
+def isvalidmove(r1,c1,r2,c2):
+    if((abs(r1-r2)==1)and(abs(c1-c2)==2))or((abs(c1-c2)==1)and(abs(r1-r2)==2)):
+        return True
+    return False
+ 
+def rowcol(board,i):
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            if (board[x][y] == i):
+                return x,y
+    return (-1,-1)
+ 
+board = [
+            [  1, 60, 39, 34, 31, 18,  9, 64 ],
+            [ 38, 35, 32, 61, 10, 63, 30, 17 ],
+            [ 59,  2, 37, 40, 33, 28, 19,  8 ],
+            [ 36, 49, 42, 27, 62, 11, 16, 29 ],
+            [ 43, 58,  3, 50, 41, 24,  7, 20 ],
+            [ 48, 51, 46, 55, 26, 21, 12, 15 ],
+            [ 57, 44, 53,  4, 23, 14, 25,  6 ],
+            [ 52, 47, 56, 45, 54,  5, 22, 13 ],
+        ]
+assert(isKnightsTour(board)==True)
+# You can write your own test cases here...
+print("All test cases passed....")
