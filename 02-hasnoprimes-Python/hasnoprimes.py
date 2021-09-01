3561fd5f1d201f0b7d578bd62a5e56f5def4be51
vinaykumar721 <gamanab163@msitprogram.net>
2021-09-01 08:56:31 +0530

    Done

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -3,5 +3,25 @@
 
 
 def fun_hasnoprimes(l):
-	return True
-
+    l1=[]
+    for i in l:
+        for j in i:
+            l1.append(j)
+ 
+    for x in l1:
+        if isPrime(x):
+            return False
+    return True
+ 
+def isPrime(n):
+    if n<2:
+        return False
+    elif n==2:
+        return True
+    elif n%2==0:
+        return False
+    else:
+        for i in range(3,n):
+            if n%i==0:
+                return False
+        return True
