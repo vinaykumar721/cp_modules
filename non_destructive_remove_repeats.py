a2641712a7d549f52ac27192bef4fe5510bb6b06
vinaykumar721 <gamanab163@msitprogram.net>
2021-09-01 08:59:49 +0530

    Done

--- non_destructive_remove_repeats.py
+++ non_destructive_remove_repeats.py
@@ -7,12 +7,7 @@
 # assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
 # assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
 # assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
-
 def nondestructiveRemoveRepeats(L):
-    # Your code goes here...
-    pass
-
-assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
-assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
-assert(nondestructiveRemoveRepeats([1, 2, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 2, 3, 5, 7])
-print ("All test cases passed....")
+   res = []
+   [res.append(x) for x in L if x not in res]
+   return res
